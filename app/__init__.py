6214a75a70f7dba6e19c1b405e0771aaf36f2b11
Denis T <tr.denis@gmail.com>
2018-07-12 18:35:57 +0300

    +

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,30 @@
-from flask import Flask
-from config import Config
+from flask import Flask, jsonify, abort
+from konfig import Config
+from werkzeug.exceptions import HTTPException, default_exceptions
 
-app = Flask(__name__)
-app.config.from_object(Config)
+from app.routes import routes_main
 
-from app import routes
+
+def JsonApp(app):
+    def error_handling(error):
+        if isinstance(error, HTTPException):
+            result = {'code': error.code, 'description': error.description, 'message': str(error)}
+        else:
+            description = abort.mapping[500].description
+            result = {'code': 500, 'description': description, 'message': str(error)}
+            resp = jsonify(result)
+            resp.status_code = result['code']
+            return resp
+
+    for code in default_exceptions.keys():
+        app.register_error_handler(code, error_handling)
+
+    return app
+
+
+app = JsonApp(Flask(__name__))
+
+config = Config('settings.ini')
+app.config.update(config.get_map('blog'))
+
+app.register_blueprint(routes_main)
--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,30 @@
-from app import app
-from flask import render_template
+from flask import Blueprint
+from flask import render_template, redirect, flash, request, jsonify
+from app.forms import LoginForm
+
+routes_main = Blueprint('routes_main', __name__)
 
 
-@app.route('/')
+@routes_main.route('/')
 def index():
     return render_template('landing/index.html', title='blog')
+
+
+@routes_main.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        flash('Logging user {}'.format(form.username.data))
+        return redirect('/')
+
+    return render_template('landing/login.html', title='Sign In', form=form)
+
+
+@routes_main.route('/ident')
+def ident_request():
+    headers = {k: v for k, v in request.headers}
+    headers.update({
+        'remote_addr': request.remote_addr
+    })
+    return jsonify(headers)
